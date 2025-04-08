910d88056d293a51fe59c2429bbeddac8b03f171
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-04-08 23:12:24 +0000

    Created sum of numbers.cpp

---
+++ sumOfNumbers.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2025 Jack Turcotte All rights reserved.
+//
+// Created by: Jack Turcotte
+// Date: April 5, 2025
+// Hangman program in C++
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int counter = 0;
+    int sum = 0;
+    std::string userNumber;
+
+    std::cout << "Enter an integer that isn't 0: ";
+    std::cin >> userNumber;
+
+    try {
+        int userNumAsInt = std::stoi(userNumber);
+
+        if (userNumAsInt > 0) {
+            while (counter < userNumAsInt) {
+                counter += 1;
+                sum += counter;
+            }
+        } else {
+            while (counter > userNumAsInt) {
+                counter -= 1;
+                sum += counter;
+            }
+        }
+        std::cout << "The sum of your numbers was " << sum << std::endl;
+    } catch (std::invalid_argument) {
+        std::cout << "That isn't an integer, you entered" << userNumber;
+    }
+}
